0471dfb3d815474df0f661e586547ada9d47d3fc
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-01-16 21:33:35 +0530

    Map in Java Script

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-let arr = ["Rushikesh", 45, "Kolegaon"];
+// Map in JS 
+//It can create new array by performing some operation on it
+// array.map(function(currentValue, index, arr), thisValue)
 
-// 1) for loop
-for(let i=0;i<arr.length; i++){
-    console.log("for loop : " + arr[i]);
-}
+const numbers = [65, 44, 12, 4];
+const newArr = numbers.map(myFunction)
 
-// 2) for each
-arr.forEach((element)=>{
-    console.log("for each:  " + element);
-})
-
-// 3) for / of loop
-for(element of arr){
-    console.log("for of :  "+ element);
-}
-
-// 4) for in loop - To iterate through object we use for in loop
-let obj = {
-    name:"Rushikesh",
-    rollNo:54,
-    homeTown:"Kolegaon"
-}
-
-for(key in obj){
-    console.log("for in key in : "+ obj[key]);
-    console.log(`for in key is ${key} & element is ${obj[key]}`)
+function myFunction(num) {
+  return console.log(num * 10);;
 }
