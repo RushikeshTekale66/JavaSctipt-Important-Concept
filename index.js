4bc2965e7ab11900e8c020a26560abc7f2c50a09
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-01-16 21:56:14 +0530

    Filter method in Java Script

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// Map in JS 
-//It can create new array by performing some operation on it
-// array.map(function(currentValue, index, arr), thisValue)
+//Filter in JavaScript
+// Filter() method creates a new array filled with elements that pass a test provided by a function.
+// The filter() method does not execute the function for empty elements.
+// The filter() method does not change the original array.
+// It return the true or false result
 
-const numbers = [65, 44, 12, 4];
-const newArr = numbers.map(myFunction)
+const ages = [32, 33, 16, 40];
+const result = ages.filter(checkAdult);
 
-function myFunction(num) {
-  return console.log(num * 10);;
+function checkAdult(age) {
+  return console.log(age >= 18);;
 }
