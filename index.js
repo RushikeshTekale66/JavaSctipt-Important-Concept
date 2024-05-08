d7950def6dfa687318d51a7c7c68fec631d466bf
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-08 09:18:21 +0530

    Destructuring with spread operator

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
-// Simple function
+// Destructuring in JS 
 
-// Without argeument
-function fun() {
-    console.log("Function without argument");
+// Array Declaration
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+
+// Assigning array value to the variables
+var [a, b, c] = arr;
+console.log(a, b, c);
+
+// assign value to spread operator
+var [a, b, c, ...rest] = arr;
+console.log(a, b, c, rest);  //a,b,c as a variable & rest as a array 
+
+// Skip the limit of spread operator
+var [a, , , ...rest] = arr;
+console.log(a, rest);
+
+// Distructuring object on the left hand side of assignment
+var { a, b } = { a: 1, b: 2 };
+console.log(a, b);
+
+// Convert arry in key value pair using spread
+var arr = [1, 2, 3];
+var obj = { ...arr };
+console.log(obj);
+
+// Passing spread to function
+var arr = [2, 3, 4]
+function sum(a, b, c) {
+    return a + b + c;
 }
+console.log(sum(...arr));
 
-fun();
-
-// Function with argument
-function average(x, y) {
-    return (x + y) / 2;
+// Value overriding using spread
+var obj = {
+    name:"Rushikesh",
+    company:"Ador Welding",
+    address:"Pune"
 }
-
-let a = 10;
-let b = 20;
-
-console.log("Average is ", average(a, b));
-
-// Arrow function
-
-let arrfun = ()=>{
-    console.log("Arrow function with out argument");
-}
-arrfun();
-
-// Arrow function with argument
-let sum = (x, y) => {
-    return x + y;
-}
-
-console.log("Sum is ", sum(a, b));
+console.log(obj); //Print obj
+console.log({...obj});
+console.log({...obj, name:"Rohan"});  //Print obj changing value
+console.log({name:"Adinath", ...obj}); //Print obj without changing value , Befor override & then print value;
