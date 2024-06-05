52dd4ab2729093331ccd88c75df3fe8b640f4fb7
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-05 15:53:42 +0530

    Node: cookies

--- index.js
+++ index.js
@@ -1,44 +1,28 @@
-// Destructuring in JS 
+const express = require ("express");
+const cookieParser = require("cookie-parser");
 
-// Array Declaration
-var arr = [1, 2, 3, 4, 5];
-console.log(arr);
+const app = express();
+app.use(cookieParser());
 
-// Assigning array value to the variables
-var [a, b, c] = arr;
-console.log(a, b, c);
-
-// assign value to spread operator
-var [a, b, c, ...rest] = arr;
-console.log(a, b, c, rest);  //a,b,c as a variable & rest as a array 
-
-// Skip the limit of spread operator
-var [a, , , ...rest] = arr;
-console.log(a, rest);
-
-// Distructuring object on the left hand side of assignment
-var { a, b } = { a: 1, b: 2 };
-console.log(a, b);
-
-// Convert arry in key value pair using spread
-var arr = [1, 2, 3];
-var obj = { ...arr };
-console.log(obj);
-
-// Passing spread to function
-var arr = [2, 3, 4]
-function sum(a, b, c) {
-    return a + b + c;
-}
-console.log(sum(...arr));
-
-// Value overriding using spread
-var obj = {
+const user = {
     name:"Rushikesh",
-    company:"Ador Welding",
-    address:"Pune"
+    roll:10
 }
-console.log(obj); //Print obj
-console.log({...obj});
-console.log({...obj, name:"Rohan"});  //Print obj changing value
-console.log({name:"Adinath", ...obj}); //Print obj without changing value , Befor override & then print value;
+
+app.get("/", (req, res)=>{
+    res.send("Site is ok");
+})
+
+// set the cookie to the client side
+app.get("/setcookie", (req, res)=>{
+    res.cookie("user", user);
+    res.send("Cookie settled")
+
+})
+
+// get the cookies form client request
+app.get("/getcookie", (req, res)=>{
+    res.send(req.cookies);
+})
+
+app.listen(5000, ()=>console.log("Site is live"));
