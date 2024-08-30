a15590caec16d36e358e21996eaa043180e1935f
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-30 09:07:15 +0530

    apply()

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ let name = {
    lname : "Tekale"
 }
 
- let getdata = function getName(){
-   console.log("Name is : " + this.fname + ", Lname : " + this.lname);
-}
-
 let name2 = {
    fname:"Rohan",
    lname : "Tekale"
 }
 
-getdata.call(name2);
-getdata.call(name);
+let getdata = function getName(hometown, city){
+   console.log("Name is : " + this.fname + ", Lname : " + this.lname + ", Hometown : " + hometown + ", City : " + city);
+}
+
+getdata.apply(name, ["Kolegaon", "Pune"]);
+getdata.apply(name2, ["Kolegaon", "Dharashiv"])
