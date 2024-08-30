16bf5fb9fdd4656c509e34a14acef0b6a93c7485
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-30 10:21:19 +0530

    currying in js

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-let name = {
-   fname : "Rushikesh",
-   lname : "Tekale"
+function add(a){
+   return function (b){
+      return function (c){
+         return a+b+b;
+      }
+   }
 }
+console.log(add(10)(20)(30));
 
-let name2 = {
-   fname:"Rohan",
-   lname : "Tekale"
-}
+//Advance writing
+let add1 = (a)=>(b)=>(c)=>{ return a+b+c};
 
-function getName(hometown, city){
-   console.log("Name is : " + this.fname + ", Lname : " + this.lname + ", Hometown : " + hometown + ", City : " + city);
-}
+let result = add(10);
+let result2 = result(20);
+console.log(result2(30));
 
 
-//Call() : It invoke a function by specifying ownwe object
-getName.call(name2, "Kolegaon", "Dharashiv");
-getName.call(name, "Kolegaon", "Pune");
 
-//apply() : It is similar as call(), but difference is the it take arguments as an array.
 
-getName.apply(name2, ["Kolegaon", "Dharashiv"]);
-getName.apply(name, ["Kolegaon", "Pune"]);
 
-//bind() : It is similar as call(), but is create a function when the function is called it set the keyword to the first argument.
 
-let bindMethod = getName.bind(name, "Kolegaon", "Pune");
-let bindMethod2 = getName.bind(name2, "Kolegaon", "Dharashiv");
-
-bindMethod();
-bindMethod2();
