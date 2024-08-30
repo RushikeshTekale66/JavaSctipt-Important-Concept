324172d7a79c8e598f9d541f415d1d6b7e96fedf
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-30 10:38:07 +0530

    Closure

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-function add(a){
-   return function (b){
-      return function (c){
-         return a+b+b;
-      }
-   }
+function add (){
+   let a = 10;
+   let b = 10;
+    return function(){
+      console.log(a+b);
+      
+    }
 }
-console.log(add(10)(20)(30));
 
-//Advance writing
-let add1 = (a)=>(b)=>(c)=>{ return a+b+c};
+let result = add();
+result();
 
-let result = add(10);
-let result2 = result(20);
-console.log(result2(30));
+
 
 
 
