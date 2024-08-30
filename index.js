e918ba3f19144a0474148e4c3f9a3a21503807bc
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-30 10:44:21 +0530

    callback function

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-function add (){
-   let a = 10;
-   let b = 10;
-    return function(){
-      console.log(a+b);
-      
-    }
+// Callback function
+function division(sum){
+   console.log(Math.floor(sum/2));
 }
 
-let result = add();
-result();
+function operation (num1, num2, operation){
+   let sum = num1 + num2;
+   operation(sum);
+}
 
-
+// Function is passed as argument to the another function
+operation(10, 20, division);
 
 
 
