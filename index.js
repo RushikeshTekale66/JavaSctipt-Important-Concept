7b91301c79d09ca79cdab2520c1b99a37b6a462c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-17 22:52:53 +0530

    map, filter, reduce

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-function add(a){
-   return function(b){
-      console.log(a+b);
-      
-   }
-}
-add(10)(10);
+let arr = [10, 20, 30, 40, 50];
+
+//Map : Perform operation & return new array
+let maparr = arr.map((values, index)=>{
+   console.log(values, index);
+   return values*values;
+})
+
+console.log(maparr);
+
+//Filter : Apply filter & return new array
+
+let filterarr = arr.filter((values)=>{
+   console.log(values>25);
+   return values>25;
+   
+})
+
+console.log(filterarr);
+
+//Reduce : Reduce the array into single value
+
+let reducearr = arr.reduce((value)=>{
+   return value+value;
+})
+
+console.log(reducearr);
+
+
+
+
