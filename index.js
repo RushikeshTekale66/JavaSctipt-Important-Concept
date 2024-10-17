90fd67a8c6ded8c8d10a0026a26ebda949d3c4c3
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-17 10:41:55 +0530

    Immediately invoked funtion

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-let a = 10;
-let b = '10';
-
-// Check the value not datatype : pass by value
-if(a==b){
-   console.log("Equal");
-}
-else{
-   console.log("Not equal");
-}
-
-//check the value & datatype also : pass by referance
-if(a===b){
-   console.log("Equal");  
-}
-else{
-   console.log("Not equal");
+(function fun(){
+   let a = 10;
+   let b = 20;
+   console.log(a+b);
    
-}
+}());
+
+(function(){
+   let a = 10;
+   let b = 20;
+   console.log(b-a);
+   
+}())
